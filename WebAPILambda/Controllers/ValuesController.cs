659264030f542a07ee350d8e1ee22b330939a975
AndriiOleksii <106560705+AndriiOleksii@users.noreply.github.com>
2022-08-05 16:04:55 +0300

    Add files via upload

---
+++ WebAPILambda/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using WebAPILambda.Models;
+using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+
+namespace WebAPILambda.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return $"Hello World from inside a lambda {DateTime.Now}";
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"You asked for {id}";
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] FileModel model)
+        {
+            var source = await Image.LoadAsync(model.File.OpenReadStream());
+
+            var memoryStream = new MemoryStream();
+            string contentType;
+
+            switch (model.ConvertType)
+            {
+                case ConvertType.BMP:
+                    await source.SaveAsBmpAsync(memoryStream);
+                    contentType = "image/bmp";
+                    break;
+                case ConvertType.GIF:
+                    await source.SaveAsGifAsync(memoryStream);
+                    contentType = "image/gif";
+                    break;
+                case ConvertType.PNG:
+                    await source.SaveAsPngAsync(memoryStream);
+                    contentType = "image/png";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            if (memoryStream.CanSeek)
+            {
+                memoryStream.Seek(0, SeekOrigin.Begin);
+            }
+
+            return File(memoryStream, contentType);
+        }
+
+    }
+}
---
+++ WebAPILambda/Models/FIleModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace WebAPILambda.Models
+{
+    public class FileModel
+    {
+        public IFormFile File { get; set; }
+        public ConvertType ConvertType { get; set; }
+    }
+
+    public enum ConvertType
+    {
+        PNG,
+        GIF,
+        BMP
+    }
+}
